f0ae97db6a6358f3c23a822d8e14e37128c96176
Julian Dibrani <JulianDibr@gmail.com>
2020-03-29 12:52:33 +0200

    Erweiterung um Tag/Stunden

--- Overview.js
+++ Overview.js
@@ -12,13 +12,26 @@
 
 (function() {
     'use strict';
+    var isOld = false;
     var buttons = $('.rlg-btn-secondary');
     var element = buttons.length - 1;
     var container = buttons[element].closest('.rlg-trade-display-container');
     var timeHeader = $(container).find('.rlg-trade-display-added').text();
+    if (timeHeader.includes("hour"))
+    {
+       isOld = true
+    }
+    else
+    {
+        isOld = false;
+    }
+
+
     timeHeader = timeHeader.replace(/\D/g,'');
 
-    if (timeHeader > Math.floor(Math.random()*(26-20+1)+20)){
+    //if (true)
+    if (timeHeader > Math.floor(Math.random()*(26-20+1)+20) || isOld)
+    {
         //if(true){
         setTimeout(function(){buttons[element].click();}, Math.floor(Math.random()*(18304-7928+1)+7928));
     } else
